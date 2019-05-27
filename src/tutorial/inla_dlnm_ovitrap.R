a7b85a8c996d490a4f0e39c56caada6e3653ad42
Kathleen Sucipto <sucipto.kathleen@gmail.com>
2019-05-27 17:52:23 +0800

    Updated inla_dlnm_ovitrap file

--- src/tutorial/inla_dlnm_ovitrap.R
+++ src/tutorial/inla_dlnm_ovitrap.R
@@ -4,9 +4,8 @@ rm(list=ls(all=TRUE))
 # select an area between: NTS, NTN, KL, HK
 area <- "NTS"
 
+# load from csv file into 'data' variable
 filePath <- paste("../../dat/monthly_data_", area, ".csv", sep="")
-
-# read from csv
 data <- read.csv(filePath, header=T)
 
 # clean data (esp. those with '#')
@@ -18,7 +17,7 @@ library("INLA")
 library("dlnm")
 library("splines")
 
-# 1a. create crossbasis variables
+# create crossbasis variables
 cb.meanTemperature <- crossbasis(data$temperature_avg, lag=8,
                                  argvar=list(fun="ns", df=3),
                                  arglag=list(fun="ns", knots=logknots(4, 1)))
@@ -36,12 +35,11 @@ getUpdatedColnames <- function(varName, oldColnames) {
   return (newColnames)
 }
 
-# 1b. update colnames
+# update colnames
 colnames(cb.meanTemperature) <- getUpdatedColnames("cb.meanTemperature", colnames(cb.meanTemperature))
 colnames(cb.totalRainfall) <- getUpdatedColnames("cb.totalRainfall", colnames(cb.totalRainfall))
 
 inla.fit <- inla(ovitrap_idx ~ cb.meanTemperature + cb.totalRainfall,
-                 family="poisson",
                  data=data,
                  control.fixed=list(correlation.matrix=T),
                  control.compute=list(dic=T, waic=T))
@@ -49,16 +47,20 @@ inla.fit <- inla(ovitrap_idx ~ cb.meanTemperature + cb.totalRainfall,
 inla.coef <- inla.fit$summary.fixed$mean
 inla.vcov <- inla.fit$misc$lincomb.derived.covariance.matrix
 
-# crosspred for cb.meanTemperature
+# obtaining values relevant to cb.meanTemperature
 cond <- paste("cb.meanTemperature","[[:print:]]*v[0-9]{1,2}\\.l[0-9]{1,2}",sep="")
 indvcov <- grep(cond, rownames(inla.vcov))
 indcoef <- grep(cond, rownames(inla.fit$summary.fixed))
 inla.coef <- inla.coef[indcoef]
 inla.vcov <- inla.vcov[indvcov,indvcov,drop=FALSE]
+
 pred.meanTemperature <- crosspred(cb.meanTemperature, at=10:30, bylag=0.2, cumul=T, coef=inla.coef, vcov=inla.vcov)
+
 plot(pred.meanTemperature, xlab="Temperature", zlab="RR", ylab="Lag")
+
 plot(pred.meanTemperature, "contour",
      plot.title = title(xlab="Temperature", ylab="Lag", main="Contour Graph"),
      key.title=title("RR"))
+
 print(inla.fit$dic$dic)
 print(inla.fit$waic$waic)
