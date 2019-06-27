0a4f492421fe6f7d55573e4cb1c6dd3c6e77a997
Kathleen Sucipto <sucipto.kathleen@gmail.com>
2019-06-27 12:29:54 +0800

    Added manual prediction for dlnm + inla

--- src/tutorial/ovitrap_inla_dlnm_prediction.R
+++ src/tutorial/ovitrap_inla_dlnm_prediction.R
@@ -23,7 +23,7 @@ cb.meanTemperature <- crossbasis(data$temperature_avg, lag=8,
                                  arglag=list(fun="ns", knots=logknots(4, 1)))
 cb.totalRainfall <- crossbasis(data$total_rain, lag=5,
                                argvar=list(fun="ns", df=3),
-                               arglag=list(fun="ns", knots=logknots(5, 1)))
+                               arglag=list(fun="ns", knots=logknots(5, 2)))
 
 getUpdatedColnames <- function(varName, oldColnames) {
   newColnames <- c()
@@ -35,6 +35,15 @@ getUpdatedColnames <- function(varName, oldColnames) {
   return (newColnames)
 }
 
+getRelevantCoefAndVcov <- function(varName, summary.fixed, coef, vcov) {
+  cond <- paste(varName,"[[:print:]]*v[0-9]{1,2}\\.l[0-9]{1,2}",sep="")
+  indvcov <- grep(cond, rownames(vcov))
+  indcoef <- grep(cond, rownames(summary.fixed))
+  coef <- coef[indcoef]
+  vcov <- vcov[indvcov,indvcov,drop=FALSE]
+  return (list("coef"=coef, "vcov"=vcov))
+}
+
 # update colnames
 colnames(cb.meanTemperature) <- getUpdatedColnames("cb.meanTemperature", colnames(cb.meanTemperature))
 colnames(cb.totalRainfall) <- getUpdatedColnames("cb.totalRainfall", colnames(cb.totalRainfall))
@@ -42,25 +51,32 @@ colnames(cb.totalRainfall) <- getUpdatedColnames("cb.totalRainfall", colnames(cb
 inla.fit <- inla(ovitrap_idx ~ cb.meanTemperature + cb.totalRainfall,
                  data=data,
                  control.fixed=list(correlation.matrix=T),
-                 control.compute=list(dic=T, waic=T))
+                 control.compute=list(dic=T, waic=T, config=T))
 
 inla.coef <- inla.fit$summary.fixed$mean
 inla.vcov <- inla.fit$misc$lincomb.derived.covariance.matrix
 
-# obtaining values relevant to cb.meanTemperature
-cond <- paste("cb.meanTemperature","[[:print:]]*v[0-9]{1,2}\\.l[0-9]{1,2}",sep="")
-indvcov <- grep(cond, rownames(inla.vcov))
-indcoef <- grep(cond, rownames(inla.fit$summary.fixed))
-inla.coef <- inla.coef[indcoef]
-inla.vcov <- inla.vcov[indvcov,indvcov,drop=FALSE]
+# obtain coef and vcov for each crossbasis variables
+info.meanTemperature <- getRelevantCoefAndVcov("cb.meanTemperature", inla.fit$summary.fixed,
+                                               inla.coef, inla.vcov)
+info.totalRainfall <- getRelevantCoefAndVcov("cb.totalRainfall", inla.fit$summary.fixed,
+                                             inla.coef, inla.vcov)
 
-pred.meanTemperature <- crosspred(cb.meanTemperature, at=10:30, bylag=0.2,
-                                  cumul=T, coef=inla.coef, vcov=inla.vcov)
 
-plot(pred.meanTemperature, xlab="Temperature", zlab="Ovitrap Index", ylab="Lag")
-plot(pred.meanTemperature, "contour",
-     plot.title = title(xlab="Temperature", ylab="Lag", main="Contour Graph"),
-     key.title=title("OI"))
+# manual prediction
+# initiate with NA (120 in total)
+predicted <- rep(NA, 120)
+inla.intercept <- inla.fit$summary.fixed$mean[1]
 
-print(inla.fit$dic$dic)
-print(inla.fit$waic$waic)
+# avoid NA between 1 - 8
+for (row_i in 9:120) {
+  temp.row <- cb.meanTemperature[row_i,]
+  rain.row <- cb.totalRainfall[row_i,]
+  
+  # following the formula ovitrap_idx ~ cb.meanTemperature + cb.totalRainfall
+  predicted[row_i] <- sum(temp.row * info.meanTemperature$coef) +
+                      sum(rain.row * info.totalRainfall$coef) + inla.intercept
+}
+predicted[predicted < 0] <- 0
+plot(data$ovitrap_idx, type="l", col="red")
+lines(predicted, col="blue")
